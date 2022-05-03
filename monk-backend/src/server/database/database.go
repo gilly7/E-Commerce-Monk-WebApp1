a8ed9576fa59f6ef220de01d493b9a68eddb3a20
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-05-03 13:33:57 +0300

    modify database object

--- monk-backend/src/server/database/database.go
+++ monk-backend/src/server/database/database.go
@@ -8,11 +8,13 @@ import (
 
 func Connection() (DB *sql.DB) {
 
-	dbDriver := "mysql"
-	dbUser := "root"
-	dbName := "monk"
+	// dbDriver := "mysql"
+	// dbUser := "root"
+	// dbName := "monk"
 
-	db, err := sql.Open(dbDriver, dbUser+":"+"@/"+dbName+"?parseTime=true")
+	//db, err := sql.Open(dbDriver,dbUser+":"+"@/"+dbName+"?parseTime=true")
+
+	db, err := sql.Open("mysql", "root:Ng@14all7@tcp(127.0.0.1:3306)/monk")
 
 	if err != nil {
 		panic(err.Error())
